b34195461eabad0fce5af699b39f137a6ef5f718
Berg Mpe <bergmpe@gmail.com>
2017-06-17 18:05:01 -0300

    added code to read test set, subimit test set to classifier, classify_nn return the name of class classified.

--- nn.pl
+++ nn.pl
@@ -2,23 +2,34 @@
 use strict;
 use warnings;
 
-my $file = 'iris.data';
+my $data_set = 'iris.data';
+my $test_set = 'iris.test';
 my %classes;
+my %test_classes;
 
-open my $info, $file or die  "Could not open the file $file";
+open my $info, $data_set or die  "Could not open the file $data_set";
+open my $test, $test_set or die  "Could not open the file $test_set";
 
+#read data set.
 while (<$info>) {
 	my ($attributes, $class_name) = split(/,I/);
 	$classes {$attributes} = "I".$class_name;
 }
 close $info;
 
-while(1){
-	print "Enter the attributes separated by , : ";
-	chomp (my $attributes = <STDIN>);
-	classify_nn($attributes);
+#read test set.
+while (<$test>) {
+	my ($attributes, $class_name) = split(/,I/);
+	$test_classes {$attributes} = "I".$class_name;
 }
- 
+close $test;
+
+#subimit the test set to classifier.
+for(keys %test_classes){
+	print "test class: $test_classes{$_}, classify: ", classify_nn($_), "\n";
+}
+
+#receive the attributes, classify and return the class name.
 sub classify_nn{
 	my ($min_distance, $min_distance_key) = 100, 0;
 	for my $key (keys %classes){
@@ -28,7 +39,7 @@ sub classify_nn{
 			$min_distance_key = $key;			
 		}
 	}
-	print "$classes{$min_distance_key}\n";
+	return $classes{$min_distance_key};
 }
 
 sub distance{
